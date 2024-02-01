e1d7ab3fef59554dd86ca831e1545b25d6d62d1b
Jeff Joji <103479461+jeffjoji6@users.noreply.github.com>
2024-02-02 01:56:12 +0530

    update1

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,13 +3,13 @@ import React from "react";
 function Sidebar() {
   return (
     <div className="">
-      <div className="h-screen w-60">
-        <div className=" py-6 text-center flex justify-center bg-[#40E0D0]">
-          <p className=" border-y-4 w-fit s border-black tracking-widest">
+      <div className="h-20 w-56">
+        <div className=" py-6 text-center flex justify-center  bg-[#40E0D0]">
+          <p className=" border-y-4 w-fit text-2xl border-black tracking-[20px]">
             ARETE
           </p>
         </div>
-        <div className="text-center flex flex-col">
+        <div className="text-center flex flex-col font-league font-semibold spartan">
           <button class="pt-10 pb-2 ">Dashboard</button>
           <button class="pt-10 pb-2 ">Ticket</button>
           <button class="pt-10 pb-2 ">Notification</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-
+      fontFamily: {
+        sans: ["League Spartan", "ui-sans-serif", "system-ui"],
+      },
     },
   },
   plugins: [],
-}
-
+};
