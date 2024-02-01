f7e04b35f679e45e75a2e22a08c7763bfbf668de
Shreyus R <104288239+Shreyus0808@users.noreply.github.com>
2024-02-02 03:36:19 +0530

    tickets template made

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,25 +6,25 @@ import img4 from "../images/image 6.png";
 
 function Sidebar() {
   return (
-    <div className="bg-white z-50">
+    <div className="bg-white z-50 shadow-lg shadow-gray-400">
       <div className="h-20 w-56 ">
         <div className=" py-6 text-center flex justify-center  bg-[#40E0D0]">
-          <p className=" border-y-4 w-fit text-2xl border-black tracking-[20px]">
+          <p className=" border-y-4 w-fit text-2xl border-black font-bold text-center tracking-[20px]">
             ARETE
           </p>
         </div>
-        <div className="text-center flex flex-col font-League font-semibold  text-lg  ">
-          <button class="py-4 border inline-flex items-center hover:bg-gray-200">
-            <img src={img1} className="p-3"></img>Dashboard
+        <div className="text-center flex flex-col font-League font-semibold space-y-2 text-lg mt-4 ">
+          <button class="py-1 px-2 inline-flex items-center mx-3 rounded-xl transition-all duration-200 text-sm font-bold  hover:bg-gray-200">
+            <img src={img1} className="p-3 w-12 h-12"></img>Dashboard
           </button>
-          <button class="py-4 border flex items-center hover:bg-gray-200">
-            <img src={img2} className="p-3"></img>Tickets
+          <button class="py-1 px-2 inline-flex items-center mx-3 rounded-xl transition-all duration-200 text-sm font-bold  hover:bg-gray-200">
+            <img src={img2} className="p-3  w-12 h-12"></img>Tickets
           </button>
-          <button class="py-4 border flex items-center  hover:bg-gray-200">
-            <img src={img3} className="p-3"></img>Notification
+          <button class="py-1 px-2 inline-flex items-center mx-3 rounded-xl transition-all duration-200 text-sm font-bold  hover:bg-gray-200">
+            <img src={img3} className="p-3  w-12 h-12"></img>Notification
           </button>
-          <button class="py-4 border flex items-center hover:bg-gray-200">
-            <img src={img4} className="p-3"></img>Setting
+          <button class="py-1 px-2 inline-flex items-center mx-3 rounded-xl transition-all duration-200 text-sm font-bold  hover:bg-gray-200">
+            <img src={img4} className="p-3  w-12 h-12"></img>Setting
           </button>
         </div>
       </div>
--- src/pages/tickets.js
+++ src/pages/tickets.js
@@ -7,15 +7,34 @@ function Tickets() {
     return (
         <>
             <div className='w-full h-screen p-4 px-6 bg-[#E9E9E9] flex flex-col divide-y-[3px]  divide-[#AAAAAA]'>
-                <div className='flex justify-between pb-1.5'>
+                <div className='flex justify-between py-2 pb-5'>
                     <h2 className=' font-sans font-bold text-3xl text-[#AAAAAA]'>Tickets</h2>
                     <img src={user} className='w-10 h-10 z-10'></img>
                 </div>
-                <div className='flex flex-col justify-between px-0 p-4'>
-                    <div className='bg-white p-2 rounded-xl'>
-
+                <div className='flex flex-col space-y-3 justify-between px-0 p-4'>
+                    <div className='bg-white flex py-4 justify-between space-x-10 px-10 rounded-xl'>
+                        <h3 className='font-bold'>ID</h3>
+                        <h3 className='font-bold px-4'>Ticket Status</h3>
+                        <h3 className='font-bold pr-40'>Description</h3>
+                        <h3 className='font-bold pr-10'>Assigned By</h3>
+                        <h3 className='font-bold pr-5'>Project</h3>
+                        <h3 className='font-bold px-5'>Priority</h3>
+                        <h3 className='font-bold '>ETA</h3>
                     </div>
+                    <div className='bg-white rounded-xl'>
+                        <table className='flex flex-col divide-y-[3px] px-5 divide-[#aaaaaa]'>
+                        <tr className='flex py-4 justify-between space-x-10 px-5'>
+                        <h3 className=''>85</h3>
+                        <h3 className=' px-4 '>Ticket Status</h3>
+                        <h3 className=' pr-40'>Description</h3>
+                        <h3 className=' pr-10'>Assigned By</h3>
+                        <h3 className=' pr-5'>Project</h3>
+                        <h3 className=' px-5'>Priority</h3>
+                        <h3 className=' '>ETA</h3>
+                        </tr>
 
+                        </table>
+                    </div>
                 </div>
 
             </div>
