4f53ba5e1bc19c8c7af90119f323afe010f9c9a1
Jeff Joji <103479461+jeffjoji6@users.noreply.github.com>
2024-02-02 08:27:14 +0530

    hello

--- src/pages/analysis.js
+++ src/pages/analysis.js
@@ -1,6 +1,7 @@
 import React from "react";
 import about from "../images/about image.png";
 import user from "../images/user.png";
+import { Chart } from "react-google-charts";
 
 function Dashboard() {
   return (
@@ -13,6 +14,26 @@ function Dashboard() {
       </div>
       <div className="flex flex-col justify-between px-0 p-4">
         <div className="bg-white p-2 rounded-xl flex items-center justify-between h-screen"></div>
+        <div style={{ display: "flex", maxWidth: 900 }}>
+          <Chart
+            width={"500px"}
+            height={"300px"}
+            chartType="PieChart"
+            loader={<div>Loading Chart</div>}
+            data={[
+              ["Task", "Hours per Day"],
+              ["Work", 11],
+              ["Eat", 2],
+              ["Commute", 2],
+              ["Watch TV", 2],
+              ["Sleep", 7],
+            ]}
+            options={{
+              title: "My Daily Activities",
+            }}
+            rootProps={{ "data-testid": "1" }}
+          />
+        </div>
       </div>
     </div>
   );
